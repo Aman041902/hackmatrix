9cabeda1b4af6bc1cdd23b99a4d0df41c6c091fc
Ratul Kulkarni <ratulkulkarni10@gmail.com>
2025-03-01 17:12:33 +0530

    First commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,17 @@
-import { useState } from "react";
+import { useState, React } from "react";
 import LandingPage from "./components/LandingPage";
 import StudentDashboard from "./components/StudentDashboard";
-import CoursePlaylistPage from "./CoursePlaylistPage";
 import ProfileProgressPage from "./components/ProfileProgressPage";
 import LeaderboardBadgeShowcase from "./components/LeaderboardBadgeShowcase";
 import TeacherDashboard from "./components/TeacherDashboard";
+import Login from "./components/Login";
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <div>
-      {/* <LandingPage></LandingPage> */}
-      {/* <StudentDashboard></StudentDashboard> */}
-      {/* <CoursePlaylistPage></CoursePlaylistPage> */}
-      {/* <LeaderboardBadgeShowcase></LeaderboardBadgeShowcase> */}
-      {/* <TeacherDashboard></TeacherDashboard> */}
-      <ProfileProgressPage></ProfileProgressPage>
+      <Login/>
+
     </div>
   );
 }
